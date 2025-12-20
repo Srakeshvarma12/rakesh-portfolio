a7524d830527d5a6fe3812243c43b39ec201815c
Rakesh Varma <srvarma2004@gmail.com>
2025-12-21 00:25:50 +0530

    Stabilize layout and fix scroll jitter (App + Framer Motion)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
-import CursorMagnet from "./components/CursorMagnet";
-import Navbar from "./components/Navbar";
-import AnimatedBackground from "./components/AnimatedBackground";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
 export default function App() {
   return (
-    <>
+    <div className="app-wrapper">
       <AnimatedBackground />
       <Navbar />
       <CursorMagnet />
 
-      {/* 🔒 GLOBAL CENTER LOCK */}
-      <main className="app-wrapper mx-auto">
+      {/* 🔒 CENTERED CONTENT */}
+      <main className="app-container">
         <Hero />
         <About />
         <Experience />
         <Projects />
         <Contact />
-        <Footer />
       </main>
-    </>
+
+      <Footer />
+    </div>
   );
 }
